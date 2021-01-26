f966038428b79c5837863e4d06ce6bcc649d7651
Luana Souza <luana.souza@ifood.com.br>
2021-01-26 14:53:57 -0300

    init Navigation Componentconfing gradle

--- app/src/main/java/br/com/luanadev/navigationcomponentapplication/MainActivity.kt
+++ app/src/main/java/br/com/luanadev/navigationcomponentapplication/MainActivity.kt
@@ -5,19 +5,11 @@ import android.os.Bundle
 import br.com.luanadev.navigationcomponentapplication.ui.profile.ProfileFragment
 import br.com.luanadev.navigationcomponentapplication.ui.start.StartFragment
 
-class MainActivity : AppCompatActivity(), StartFragment.OnButtonClicked {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.my_container, StartFragment.newInstance())
-            .commit()
     }
 
-    override fun buttonClicked() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.my_container, ProfileFragment.newInstance())
-            .commit()
-    }
 }
